3ebf64e67b8fbbf9e6ac311e75e84ed18c75f7e1
Danny Robinson <daniel@djrobinson.me>
2016-01-20 10:45:01 -0700

    higher order partial function

---
+++ higher-order.js
@@ -0,0 +1,34 @@
+var letters = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+  'o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+// functional + higherOrderFunctions
+function getVowels(arr) {
+  // loop through `letters`, passing each letter to `isVowel()`
+  var vowelsArray = arr.filter(function(letter){
+    console.log('return ' + isVowel(letter));
+    return isVowel(letter);
+  });
+  return vowelsArray;
+}
+
+
+//use filter on the example below
+function isVowel(letter) {
+  // return true if a letter is a vowel
+  var vowels = ['a','e','i','o','u'];
+  vowels.filter(function(vowel){
+    console.log("vowel: " + vowel, "letter: " + letter, "truthy: " + (vowel === letter));
+    return (vowel === letter);
+  });
+
+  // for (var i = 0; i < vowels.length; i++) {
+  //   if(vowels[i] === letter) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+}
+
+console.log(getVowels(letters));
